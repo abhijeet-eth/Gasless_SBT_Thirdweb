284b953d470e63f8c4da741e294295b89945c58e
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2023-06-13 20:44:06 +0530

    ipfs feature added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { create as ipfsHttpClient } from "ipfs-http-client";
 import {
   ThirdwebProvider,
   useContract,
@@ -8,7 +9,15 @@ import {
 } from "@thirdweb-dev/react";
 import { mumbai } from "@thirdweb-dev/chains";
 
+const projectId = "2JwnITRYL8qxpSsA0lp5yXRGk8m";
+const projectSecretKey = "ad81c7eb6e7578e078eaed4994f5f2f6";
+const authorization = "Basic " + btoa(projectId + ":" + projectSecretKey);
+var ipfsPath;
+
 export default function Component() {
+  const addr = useAddress();
+  console.log("addr =>", addr);
+
   const { contract } = useContract(
     "0xE267ee10D2E929b4Ed5D9C39186fc637F2a51c88"
   );
@@ -17,19 +26,67 @@ export default function Component() {
     "safeMint"
   );
 
-  const call = async () => {
-    try {
-      const data = await safeMint({
-        args: ["0xE267ee10D2E929b4Ed5D9C39186fc637F2a51c88", "uri"],
-      });
-      console.info("contract call successs", data);
-    } catch (err) {
-      console.error("contract call failure", err);
+  const ipfs = ipfsHttpClient({
+    url: "https://ipfs.infura.io:5001/api/v0",
+    headers: {
+      authorization,
+    },
+  });
+
+  const call = async (event) => {
+
+
+    event.preventDefault();
+    const form = event.target;
+    const files = form[0].files;
+
+    if (!files || files.length === 0) {
+      return alert("No files selected");
     }
+
+    const file = files[0];
+    // upload files
+    const result = await ipfs.add(file);
+    const completeImagePath = `https://infura-ipfs.io/ipfs/${result.path}`;
+
+    var metaData = {
+      address: addr,
+      name: "CG UK NFT Campaign",
+      attributes: [
+        {
+          trait_type: "Batch",
+          value: "First Batch",
+        },
+        {
+          trait_type: "Organizer",
+          value: "CG UK Invent Team",
+        },
+        {
+          trait_type: "Partner",
+          value: "CG Indian Invent Team",
+        },
+      ],
+      image: completeImagePath,
+    };
+
+    const metaDataUri = JSON.stringify(metaData);
+    console.log("metaDatUri ==> ", metaDataUri);
+
+    const result2 = await ipfs.add(metaDataUri);
+    // console.log("result2 ==>", result2)
+    ipfsPath = `https://infura-ipfs.io/ipfs/${result2.path}`;
+    console.log("Metadata URI ==>", ipfsPath);
+
+    // try {
+    //   const data = await safeMint({
+    //     args: [addr, completeMetadataPath],
+    //   });
+    //   console.info("contract call successs", data);
+    // } catch (err) {
+    //   console.error("contract call failure", err);
+    // }
   };
 
-  const addr = useAddress();
-  console.log("addr =>", addr);
   return (
     <ThirdwebProvider
       activeChain="mumbai"
@@ -47,7 +104,7 @@ export default function Component() {
         {/* <Component /> */}
         <main className="main">
           <h1 className="title">
-            Welcome to <a href="https://thirdweb.com/">NFT Capaiggn</a>!
+            Welcome to <a href="https://thirdweb.com/">Gasless Soulbound NFT Mint</a>!
           </h1>
 
           <div className="connect">
@@ -55,14 +112,23 @@ export default function Component() {
               dropdownPosition={{ side: "bottom", align: "center" }}
             />
           </div>
-          <Web3Button
+          {ipfs ? (
+            <form class="form" onSubmit={call}>
+              <label for="file-upload" class="custom-file-upload">
+                Select File
+              </label>
+              <input id="file-upload" type="file" name="file" />
+            
+          <Web3Button 
             contractAddress="0xE267ee10D2E929b4Ed5D9C39186fc637F2a51c88"
             action={(contract) => {
-              contract.call("safeMint", [addr, "uri"]);
+              contract.call("safeMint", [addr, ipfsPath]);
             }}
           >
             safeMint
           </Web3Button>
+          </form>
+          ) : null}
         </main>
       </div>
     </ThirdwebProvider>
